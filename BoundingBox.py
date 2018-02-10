3591c266cd617c81f3341283ec199c3452847bd8
DESKTOP-MMMK4EJ\duong <duongnhatthang@gmail.com>
2018-02-10 22:16:25 +0700

    -Add read Input

--- BoundingBox.py
+++ BoundingBox.py
@@ -1,25 +1,23 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-N = 20
-x = np.random.rand(N)
-y = np.random.rand(N)
+input = np.loadtxt("testInput.txt", dtype='i', delimiter=' ')
 xMax = 0
 yMax = 0
 xMin = 999
 yMin = 999
-for i in range(N):
-    if x[i]>xMax:
-        xMax = x[i]
-    if y[i]>yMax:
-        yMax = y[i]
-    if x[i]<xMin:
-        xMin = x[i]
-    if y[i]<yMin:
-        yMin = y[i]
-plt.scatter(x,y)
-plt.plot([xMin,xMin],[yMin,yMax])
-plt.plot([xMin,xMax],[yMax,yMax])
-plt.plot([xMax,xMax],[yMax,yMin])
-plt.plot([xMax,xMin],[yMin,yMin])
+for i in range(int(input.size/2)-1):
+    if input[i][0]>xMax:
+        xMax = input[i][0]
+    if input[i][1]>yMax:
+        yMax = input[i][1]
+    if input[i][0]<xMin:
+        xMin = input[i][0]
+    if input[i][1]<yMin:
+        yMin = input[i][1]
+plt.scatter(input[:,0],input[:,1])
+plt.plot([xMin,xMin],[yMin,yMax],'r')
+plt.plot([xMin,xMax],[yMax,yMax],'r')
+plt.plot([xMax,xMax],[yMax,yMin],'r')
+plt.plot([xMax,xMin],[yMin,yMin],'r')
 plt.show()
